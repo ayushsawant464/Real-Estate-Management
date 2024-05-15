b9a1336bca6f68685aedc9f13251fd897aea8ae0
ayush sawant <ayushsawant034@gmail.com>
2024-05-15 14:26:25 +0530

    added Readme.md

---
+++ routes/property.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {  } = require("../utils/verifyUser.js");
+const { createProperty, deleteProperty, updateProperty, getProperty, getProperties } = require("../controllers/property.controller.js");
+
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello from property routes");
+});
+router.post("/create", createProperty);
+router.delete("/delete/:id", deleteProperty);
+router.post("/update/:id" , updateProperty);
+router.get("/get/:id", getProperty);
+router.get("/get", getProperties);
+
+module.exports = router;
+
---
+++ routes/user.routes.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const { test,updateUser, deleteUser, getUserListings, getUser } = require("../controllers/user.controller.js");
+const {  } = require("../utils/verifyUser.js");
+
+const router = express.Router();
+
+
+router.get('/test', test);
+router.post("/update/:id",  updateUser);
+router.delete("/delete/:id",  deleteUser);
+router.get("/listings/:id",  getUserListings);
+router.get("/:id",  getUser);
+
+module.exports=router;
